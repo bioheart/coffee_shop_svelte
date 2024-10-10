7a50df7531060b5b1b6db74381dbc33c386f6133
bioheart.kalel@gmail.com <tae.panyaphoem@everymatrix.com>
2024-10-10 16:42:29 +0700

    first commit

---
+++ src/components/header.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { page } from '$app/stores';
+    const menus = [{
+            title:'Home',
+            url:'/'
+        },
+        {
+            title:'About',
+            url:'/about'
+        },
+        {
+            title:'Contact',
+            url:'/contact'
+        }
+    ]
+</script>
+<nav class="flex flex-row w-full">
+    <div class="w-5/12"><a href="/">LOGO</a></div>
+    <ul class="flex flex-row w-6/12">
+        {#each menus as item}
+            <li class="mx-auto">
+                <a href={item.url} class='text-orange-300 hover:text-orange-600 {item.url===$page.url?.pathname?'active':''}'>{item.title}</a>
+            </li>
+        {/each}
+    </ul>
+    <div class="social-icons">
+        <a href="https://instagram.com">IG</a>
+    </div>
+</nav>
+
+<style>
+    .active {
+        @apply text-orange-900; /* Change the background color and text color */
+    }
+    .social-icons {
+        @apply flex w-1/12 mx-auto w-5;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import Header from "../components/header.svelte"
+
+</script>
+
+<div class="cointainer mx-auto">
+    <div class="px-48 xl:px-96">
+        <Header/>
+        <slot/>
+    </div>
+</div>
